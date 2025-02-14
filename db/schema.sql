ed7e4ebc2a3539fb32233642a938e6b465bb48f1
Your Name <marielmetalios@gmail.com>
2025-02-14 07:39:03 -0500

    initialized project with required npm packages, created schema and connection

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+\c courses_db;
+
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+)
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DEC,
+    department_id INT REFERENCES department(id) ON DELETE SET NULL
+)
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INT REFERENCES employee(id) ON DELETE SET NULL
+)
---
+++ src/connection.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config;
+
+import pg from 'pg';
+const { Pool } = pg;
+
+const pool = new Pool({
+    user: process.env.DB_user,
+    password: process.env.DB_password,
+    host: 'localhost',
+    database: process.env.DB_name,
+    port: 5432
+})
+
+const connectToDb = async () => {
+    try {
+      await pool.connect();
+      console.log('Connected to the database.');
+    } catch (err) {
+      console.error('Error connecting to database:', err);
+      process.exit(1);
+    }
+  };
+
+export { pool }
