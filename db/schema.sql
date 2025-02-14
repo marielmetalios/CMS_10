b1365d1229bfe6dfb160243f89cfbd0691299ccc
Your Name <marielmetalios@gmail.com>
2025-02-14 14:46:19 -0500

    updating schema and seed files

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
-\c courses_db;
+\c company_db;
 
 CREATE TABLE department (
   id SERIAL PRIMARY KEY,
@@ -12,13 +12,13 @@ CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DEC,
-    department_id INT REFERENCES department(id) ON DELETE SET NULL
+    department_id INT, FOREIGN KEY (department) REFERENCES department(id) ON DELETE SET NULL
 )
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
-    role_id INT REFERENCES role(id) ON DELETE SET NULL,
+    role_id INT FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     manager_id INT REFERENCES employee(id) ON DELETE SET NULL
 )
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,28 @@
 import { pool } from "./connection.js"
 
+INSERT INTO department (id, name)
+VALUES 
+(1, 'Marketing'),
+(2, 'Accounting'),
+(3, 'Legal'),
+(4, 'Human Resources'),
+(5, 'Sales'),
+(6, 'IT');
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES 
+(1, 'Social Media Manager', 80000, 1),;
+(2, 'CPA', 150000, 2),;
+(3, 'Compliance Attorney', 200000, 3),;
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES 
+(1, 'Trisha', 'Taylor', 1, 5);
+(2, 'John', 'Smith', 2, 6);
+(3, 'Michael', 'Doe', 3, 4, NULL);
+(4, 'Lindsay', 'Michaels', 3, 6)
+(5, 'Christina', 'Devon', 1, NULL),;
+(6, 'Kevin', 'Swift', 2, 3),;
+
+
 
